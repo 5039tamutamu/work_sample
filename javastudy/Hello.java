ccbee7693e58a8c57d94594a8a20374f1209bcb7
5039tamutamu <5039tamutamu@gmail.com>
2025-10-19 15:11:02 +0900

    java遊び

--- javastudy/Hello.java
+++ javastudy/Hello.java
@@ -1,5 +1,11 @@
 public class Hello{
 	public static void main(String args[]){
 		System.out.println("Hello World!");
+		System.out.println(funcTest(2, 3));
 	}
+	public static int funcTest(int a, int b){
+		int c = 0;
+		return c = a + b;
+	}
+
 }
